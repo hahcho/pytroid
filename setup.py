965986d890cab4c1e25356ccee9f24ef2fa1885f
Mario Daskalov <mario@browswave.com>
2015-04-12 11:35:44 +0300

    Initial package configuration

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='sample',
+    version='0.0.1a1',
+    description='Emulator for NES games written in Python',
+    long_description='',
+    url='https://github.com/hahcho/pytroid',
+    author='Mario Daskalov',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='nes emulator nintendo',
+    packages=find_packages(),
+)
