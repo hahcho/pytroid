978082105ab243b50a3d8d3623681e894199f6d7
Mario Daskalov <mariodaskalov@gmail.com>
2016-05-25 18:23:36 +0300

    Update setup.py information

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 setup(
-    name='sample',
-    version='0.0.1a1',
+    name='pytroid',
+    version='0.0.1',
     description='Emulator for NES games written in Python',
     long_description='',
     url='https://github.com/hahcho/pytroid',
@@ -16,7 +16,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
     keywords='nes emulator nintendo',
     packages=find_packages(),
